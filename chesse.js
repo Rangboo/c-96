6af064f2317412538300023f89ccef050013e637
Rangboo <93071040+Rangboo@users.noreply.github.com>
2022-08-26 11:37:52 +0800

    Add files via upload

---
+++ chesse.js
@@ -0,0 +1,9 @@
+localStorage.getItem("user_name");
+window.location("kwitter_room.html");
+
+function addUser()
+{
+  user_name=document.getElementById("user_name").value;
+  localStorage.setItem("user_name",user_name);
+  window.location="cheese_room.html";
+}
